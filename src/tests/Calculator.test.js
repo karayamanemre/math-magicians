89eff9c03dbae0f81e8ea4a5395ecc6557481f24
Emre Karayaman <karayaman.emre@outlook.com>
2022-11-01 20:18:55 +0300

    create header and quote tests

--- src/tests/Calculator.test.js
+++ src/tests/Calculator.test.js
@@ -1,8 +1,8 @@
 import { render } from '@testing-library/react';
 import Calculator from '../components/pages/Calculator';
 
-describe('test for creating', () => {
-  it('renders correctly', () => {
+describe('test for creating the calculator', () => {
+  it('created calculator', () => {
     const view = render(<Calculator />);
     expect(view).toMatchSnapshot();
   });
