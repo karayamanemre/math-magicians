93f337f013764deeaafc97a7e1006fdae833836a
Emre Karayaman <karayaman.emre@outlook.com>
2022-11-02 18:18:51 +0300

    update style

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -4,20 +4,9 @@ const Home = () => (
   <div className="home">
     <h2>Welcome to Math Magicians</h2>
     <p>
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequatur fuga
-      laboriosam quasi minus obcaecati quod unde iusto repellat, expedita
-      delectus alias dolore inventore aperiam exercitationem quas eveniet magni
-      necessitatibus. Fugiat, doloribus, quisquam atque autem incidunt placeat
-      repellat excepturi quibusdam corrupti praesentium repudiandae recusandae
-      aperiam totam quis libero alias, modi vitae!
-    </p>
-    <p>
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequatur fuga
-      laboriosam quasi minus obcaecati quod unde iusto repellat, expedita
-      delectus alias dolore inventore aperiam exercitationem quas eveniet magni
-      necessitatibus. Fugiat, doloribus, quisquam atque autem incidunt placeat
-      repellat excepturi quibusdam corrupti praesentium repudiandae recusandae
-      aperiam totam quis libero alias, modi vitae!
+      "Math magicians" is a website for all fans of mathematics.<br></br>It is a
+      Single Page App that allows users to make simple calculations, and read a
+      random math-related quote.
     </p>
   </div>
 );
